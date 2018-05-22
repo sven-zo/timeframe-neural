8d656f03c62730297787a8996923a53b604334c5
sven-zo <svenzo@outlook.com>
2018-05-22 11:05:26 +0200

    feat: adds width, height, depth

--- timeframe-alpha/script.js
+++ timeframe-alpha/script.js
@@ -1,7 +1,6 @@
-
 const synaptic = window.synaptic;
 
-const network = new synaptic.Architect.Perceptron(2, 5, 1);
+const network = new synaptic.Architect.Perceptron(5, 3, 1);
 
 const trainer = new synaptic.Trainer(network);
 
@@ -9,7 +8,7 @@ const trainer = new synaptic.Trainer(network);
 const importData = new Promise(async (resolve, reject) => {
   console.log('Importing data...');
   try {
-    const request = await fetch('mock-data/mock-1.json');
+    const request = await fetch('mock-data/MOCK_DATA.json');
     const data = await request.json();
     resolve(data);
   } catch (err) {
@@ -21,16 +20,22 @@ const formatData = new Promise(async (resolve, reject) => {
   const data = await importData;
   console.log('Formatting data...');
 
-  const mappedData = data.packages.map(package => {
+  const mappedData = data.deliveries[0].packages.map(package => {
     const fragile = package.fragile ? 1 : 0;
     return {
       input: [
-        // Gewicht
-        package.weight / 100,
+        // Weight
+        package.weight / 68,
+        // Width
+        package.dimensions.width / 274,
+        // Height
+        package.dimensions.height / 274,
+        // Depth
+        package.dimensions.depth / 274,
         // Fragile
-        fragile, 
+        fragile,
       ],
-      output: [package.time_since_last_package / 3600]
+      output: [package.delivery_delay / 3600]
     }
   });
 
@@ -43,7 +48,7 @@ const train = async (trainer) => {
     iterations: 1000,
     error: 0.005,
     shuffle: true,
-    log: 1000,
+    log: 100,
     cost: synaptic.Trainer.cost.CROSS_ENTROPY,
   }
   const data = await formatData;
@@ -58,18 +63,31 @@ const loop = () => {
 
   const weightSlider = document.getElementById('weight');
   const weightValue = document.getElementById('weight_value');
+  const widthSlider = document.getElementById('width')
+  const widthValue = document.getElementById('width_value');
+  const heightSlider = document.getElementById('height');
+  const heightValue = document.getElementById('height_value');
+  const depthSlider = document.getElementById('depth');
+  const depthValue = document.getElementById('depth_value');
   const fragile = document.getElementById('fragile');
   const score = document.getElementById('score');
   const scoreMinutes = document.getElementById('score-minutes');
   const scoreHour = document.getElementById('score-hour');
+  const image = document.querySelector('.output img');
 
   const fragileValue = fragile.checked ? 1 : 0;
 
-  weightValue.innerHTML = weightSlider.value;
-  const output = network.activate([weightSlider.value, fragileValue]);
-  score.innerHTML = `${output * 3600} seconds`;
-  scoreMinutes.innerHTML = `${output * 60} minutes`;
-  scoreHour.innerHTML = `${output} hours`;
+  weightValue.innerText = `${weightSlider.value} kg`;
+  widthValue.innerText = `${widthSlider.value} cm`;
+  heightValue.innerText = `${heightSlider.value} cm`;
+depthValue.innerText = `${depthSlider.value} cm`;
+
+  image.style.transform = `scale(${weightSlider.value / 68 * 6})`;
+
+  const output = network.activate([weightSlider.value / 68, widthSlider.value / 274, heightSlider.value / 274, depthSlider.value / 274, fragileValue]);
+  score.innerText = `${output * 3600} seconds`;
+  scoreMinutes.innerText = `${output * 60} minutes`;
+  scoreHour.innerText = `${output} hours`;
 
   requestAnimationFrame(loop);
 }
